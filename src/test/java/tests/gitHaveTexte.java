937b302595899890e61a0e53e31f852119b5a44c
Andrey <zizkun.av@list.ru>
2024-05-24 15:04:14 +0400

    Create project

--- src/test/java/tests/gitHaveTexte.java
+++ src/test/java/tests/gitHaveTexte.java
@@ -10,7 +10,7 @@ import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
-
+import static java.lang.String.format;
 import org.openqa.selenium.Cookie;
 
 @DisplayName("тест")
@@ -19,6 +19,8 @@ public class gitHaveTexte extends TestBase {
     @DisplayName("Авторизация пользователя")
     @Test
     void successfulLoginTest() {
+
+//авторизация
         String authorizationData = "{\"userName\":\"testtestov31\",\"password\":\"Testtestov31_%\"}";
         Response authResponse = given()
                 .log().uri()
@@ -38,6 +40,49 @@ public class gitHaveTexte extends TestBase {
         getWebDriver().manage().addCookie(new Cookie("userID", authResponse.path("userId")));
         getWebDriver().manage().addCookie(new Cookie("expires", authResponse.path("expires")));
         getWebDriver().manage().addCookie(new Cookie("token", authResponse.path("token")));
+//добавление
+
+        String isbn = "9781449365035";
+        String bookData = format("{\"userId\":\"%s\",\"collectionOfIsbns\":[{\"isbn\":\"%s\"}]}",
+                authResponse.path("userId") , isbn);
+
+        given()
+                .log().uri()
+                .log().method()
+                .log().body()
+                .contentType(JSON)
+                .header("Authorization", "Bearer " + authResponse.path("token"))
+                .body(bookData)
+                .when()
+                .post("/BookStore/v1/Books")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(201);
+
+        open("/favicon.ico");
+        getWebDriver().manage().addCookie(new Cookie("userID", authResponse.path("userId")));
+        getWebDriver().manage().addCookie(new Cookie("expires", authResponse.path("expires")));
+        getWebDriver().manage().addCookie(new Cookie("token", authResponse.path("token")));
+
+        open("/profile");
+        $(".ReactTable").shouldHave(text("Speaking JavaScript"));
+
+
+//удаление
+        given()
+                .log().uri()
+                .log().method()
+                .log().body()
+                .contentType(JSON)
+                .header("Authorization", "Bearer " + authResponse.path("token"))
+                .queryParams("UserId", authResponse.path("userId"))
+                .when()
+                .delete("/BookStore/v1/Book")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(204);
 
     }
 }
