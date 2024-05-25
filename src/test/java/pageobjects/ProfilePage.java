87b3b728c7f1f624b616a40cce1cb320de6c8a71
Andrey <zizkun.av@list.ru>
2024-05-25 19:13:20 +0400

    Create project

--- src/test/java/pageobjects/ProfilePage.java
+++ src/test/java/pageobjects/ProfilePage.java
@@ -1,4 +1,42 @@
 package pageobjects;
+import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
+
+import static com.codeborne.selenide.Condition.hidden;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class ProfilePage {
+
+    private final SelenideElement userName = $("#userName-value"),
+            tableOfBook = $(".rt-tbody"),
+            linkBook = $("[href='/profile?book=9781449325862']");
+
+    @Step("Open profile page")
+    public ProfilePage openProfilePage() {
+        open("/profile");
+
+        return this;
+    }
+
+    @Step("Пользователь авторизован - имя пользователя отображается на странице")
+    public ProfilePage checkUserName() {
+        userName.shouldHave(text("First"));
+
+        return this;
+    }
+    @Step("Пользователь авторизован - имя пользователя отображается на странице")
+    public ProfilePage checkNameBookInList() {
+        tableOfBook.shouldNotHave(text("Speaking JavaScript"));
+
+        return this;
+    }
+
+    @Step("Пользователь авторизован - имя пользователя отображается на странице")
+    public ProfilePage checkBook() {
+        linkBook.shouldHave(hidden);
+
+        return this;
+    }
 }
--- src/test/java/tests/DeleteBookInStoreTest.java
+++ src/test/java/tests/DeleteBookInStoreTest.java
@@ -4,6 +4,7 @@ import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Cookie;
+import pageobjects.ProfilePage;
 
 import static com.codeborne.selenide.Condition.hidden;
 import static com.codeborne.selenide.Condition.text;
@@ -13,11 +14,12 @@ import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
 import static java.lang.String.format;
 
-public class DeleteBookInStoreTest extends TestBase{
+public class DeleteBookInStoreTest extends TestBase {
+    ProfilePage profilepage = new ProfilePage();
 
     @DisplayName("Авторизация пользователя")
     @Test
-    void addBookToCollectionTest()   {
+    void addBookToCollectionTest() {
 
 // Авторизация пользователя - post
 
@@ -40,7 +42,7 @@ public class DeleteBookInStoreTest extends TestBase{
 
         String isbn = "9781449365035";
         String bookData = format("{\"userId\":\"%s\",\"collectionOfIsbns\":[{\"isbn\":\"%s\"}]}",
-                authResponse.path("userId") , isbn);
+                authResponse.path("userId"), isbn);
 
 // Добавляем книгу пользователю post
 
@@ -82,10 +84,9 @@ public class DeleteBookInStoreTest extends TestBase{
         getWebDriver().manage().addCookie(new Cookie("expires", authResponse.path("expires")));
         getWebDriver().manage().addCookie(new Cookie("token", authResponse.path("token")));
 
-        open("/profile");
-        $("#userName-value").shouldHave(text("First"));
-        $(".rt-tbody").shouldNotHave(text("Speaking JavaScript"));
-        $("[href='/profile?book=9781449325862']").shouldHave(hidden);
-
+        profilepage.openProfilePage()
+                .checkUserName()
+                .checkNameBookInList()
+                .checkBook();
     }
 }
